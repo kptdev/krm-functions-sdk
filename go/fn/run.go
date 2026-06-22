609e4f9abfbb728866e6c300eb8bbc6483a6c6a7
Mózes László Máté <laszlo.mozes@nokia.com>
2026-06-22 10:55:46 +0200

    allow explicit ResourceListProcessorFunc, add test
    Signed-off-by: Mózes László Máté <laszlo.mozes@nokia.com>

--- go/fn/run.go
+++ go/fn/run.go
@@ -86,12 +86,11 @@ func AsMain(input any, opts ...Option) error {
 
 	err := func() error {
 		var p ResourceListProcessor
-		switch input := input.(type) {
-		case runnerProcessor:
-			p = input
-		case ResourceListProcessorFunc:
-			p = input
-		default:
+		if cast, ok := input.(ResourceListProcessor); ok {
+			p = cast
+		} else if cast, ok := input.(func(*ResourceList) (bool, error)); ok {
+			p = ResourceListProcessorFunc(cast)
+		} else {
 			return fmt.Errorf("unknown input type %T", input)
 		}
 
@@ -208,14 +207,6 @@ func readFilesAsResourceList(paths []string) (*ResourceList, error) {
 // Run evaluates the function. input must be a resourceList in yaml format. An
 // updated resourceList will be returned.
 func Run(p ResourceListProcessor, input []byte) ([]byte, error) {
-	switch input := p.(type) {
-	case runnerProcessor:
-		p = input
-	case ResourceListProcessorFunc:
-		p = input
-	default:
-		return nil, fmt.Errorf("unknown input type %T", input)
-	}
 	rl, err := ParseResourceList(input)
 	if err != nil {
 		return nil, err
